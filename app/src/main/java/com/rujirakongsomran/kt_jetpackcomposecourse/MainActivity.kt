03aa3d5aa228af47bcf88eb5d2d59f83821cd46c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-08 14:54:00 +0700

    Modifier

--- app/src/main/java/com/rujirakongsomran/kt_jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_jetpackcomposecourse/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -20,6 +22,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+//                    .padding(16.dp)
+//                    .padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .width(600.dp)
+//                    .requiredWidth(600.dp)
+            ) {
+//                Text("Hello", modifier = Modifier.offset(0.dp, 20.dp))
+//                Text(
+//                    "Hello", modifier = Modifier
+//                        .border(5.dp, Color.Yellow)
+//                        .padding(5.dp)
+//                        .offset(20.dp, 20.dp)
+//                        .border(10.dp, Color.Black)
+//                        .padding(10.dp)
+//                )
+                Text("Hello", modifier = Modifier.clickable {
+
+                })
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
+            }
 //            Column(
 //                modifier = Modifier
 //                    .fillMaxSize()
@@ -31,20 +64,22 @@ class MainActivity : ComponentActivity() {
 //                Text("World")
 //                Text("Hello")
 //            }
-            Row(
-                modifier = Modifier
-//                    .fillMaxSize(0.5f)
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-//                    .height(300.dp)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text("Hello")
-                Text("World")
-                Text("Hello")
-            }
+//            Row(
+//                modifier = Modifier
+////                    .fillMaxSize(0.5f)
+//                    .width(300.dp)
+//                    .fillMaxHeight(0.7f)
+////                    .height(300.dp)
+//                    .background(Color.Green),
+//                horizontalArrangement = Arrangement.SpaceAround,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Text("Hello")
+//                Text("World")
+//                Text("Hello")
+//            }
+
+
         }
     }
 }
