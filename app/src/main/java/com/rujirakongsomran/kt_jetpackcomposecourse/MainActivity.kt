3b739eac4da3a4046f00970c23608df6c2ff0b62
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-08 12:58:53 +0700

    Text

--- app/src/main/java/com/rujirakongsomran/kt_jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_jetpackcomposecourse/MainActivity.kt
@@ -14,12 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KT_JetpackComposeCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "Biwberry")
         }
     }
 }
@@ -32,7 +27,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    KT_JetpackComposeCourseTheme {
-        Greeting("Android")
-    }
+    Greeting("Biwberry")
 }
